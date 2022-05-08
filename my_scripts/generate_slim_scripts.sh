8ef0b1a9ca294d3376e75789da093522fd346858
Yaotian_Liu <2416224849@qq.com>
2022-05-08 16:58:40 +0800

    update scripts

--- my_scripts/generate_slim_scripts.sh
+++ my_scripts/generate_slim_scripts.sh
@@ -7,14 +7,14 @@ else
     touch slim_scripts
 fi
 
-echo python tools/train.py -c $MODEL_CONFIG --slim_config $SLIM_CONFIG >> slim_scripts
+echo python tools/train.py -c $MODEL_CONFIG --slim_config $SLIM_CONFIG \\>> slim_scripts
 echo >> slim_scripts
-echo python tools/eval.py -c $MODEL_CONFIG --slim_config $SLIM_CONFIG \ >> slim_scripts
-echo    -o weights=output/$SLIM_CONFIG/model_final >> slim_scripts
+echo python tools/eval.py -c $MODEL_CONFIG --slim_config $SLIM_CONFIG \\ >> slim_scripts
+echo    -o weights=^^ >> slim_scripts
 echo >> slim_scripts
-echo python tools/infer.py -c $MODEL_CONFIG --slim_config $SLIM_CONFIG \ >> slim_scripts
-echo     -o weights=output/!!!/model_final >> slim_scripts
+echo python tools/infer.py -c $MODEL_CONFIG --slim_config $SLIM_CONFIG \\ >> slim_scripts
+echo     -o weights=^^ \\ >> slim_scripts
 echo     --infer_img=IMAGE_PATH >> slim_scripts
 echo >> slim_scripts
-echo python tools/export_model.py -c $MODEL_CONFIG --slim_config $SLIM_CONFIG \ >> slim_scripts
-echo     -o weights=output/!!!/model_final >> slim_scripts
+echo python tools/export_model.py -c $MODEL_CONFIG --slim_config $SLIM_CONFIG \\ >> slim_scripts
+echo     -o weights=^^ >> slim_scripts
