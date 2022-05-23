d206e1d70a3de68744ffdd481ab81800ad4195b7
Yaotian_Liu <2416224849@qq.com>
2022-05-23 21:55:20 +0800

    update sense graph

--- extended_test/sense_graph.py
+++ extended_test/sense_graph.py
@@ -5,8 +5,19 @@ sensitivity = pd.read_csv("./extended_test/sensitivity.csv")
 
 pruned_params = sensitivity['param'].unique()
 
+fig, ax = plt.subplots()
+
+x = sensitivity['ratio'].unique()
+
 for param in pruned_params:
     single_param = sensitivity[sensitivity['param'] == param]
-    single_param = single_param.set_index(keys=['ratio'])
+    y = single_param['sensitivity']
 
+    ax.plot(x, y, label=param)
     print(single_param)
+
+ax.set_xlabel("prune ratio")
+ax.set_ylabel("sensitivity")
+ax.legend()
+
+plt.show()
